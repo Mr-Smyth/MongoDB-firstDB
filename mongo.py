4e605ceabbe1eb81d440773164eda59ee45d01ea
Eamonn Smyth <e.smyth75@gmail.com>
2020-08-27 09:21:39 +0000

    Modify mongo.py to insert one record

--- mongo.py
+++ mongo.py
@@ -22,6 +22,18 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
+# when adding key value pairs from python, the keys also need quotes.
+new_doc = {
+    "first": "douglas",
+    "last": "adams",
+    "dob": "11/03/1952",
+    "hair_colour": "grey",
+    "occupation": "writer",
+    "nationality": "british"
+}
+
+coll.insert(new_doc)
+
 documents = coll.find()
 # This will return a mongoDB object aka a cursor.
 for doc in documents:
