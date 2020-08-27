c1d85ae1e9d588083ea794dc2a6176b650016cff
Eamonn Smyth <e.smyth75@gmail.com>
2020-08-27 09:56:13 +0000

    Updated mongo.py with Lesson on how to perform CRUD on Mongo via Python

--- mongo.py
+++ mongo.py
@@ -22,19 +22,33 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
-# when adding key value pairs from python, the keys also need quotes.
-new_doc = {
-    "first": "douglas",
-    "last": "adams",
-    "dob": "11/03/1952",
-    "hair_colour": "grey",
-    "occupation": "writer",
-    "nationality": "british"
-}
+"""
 
-coll.insert(new_doc)
+To find someone in the database with the first name of “douglas”:
+•	documents = coll.find({"first": "douglas"})
 
-documents = coll.find()
+To remove someone from the database with the first name of “douglas”:
+•	coll.remove({"first": "douglas"})
+
+To update one record in the database:
+•	coll.update_one({"nationality": "american"},
+                    {"$set": {"hair_colour": "maroon"}})
+•	This above code will update the first record it
+    reads with the nationality of american.
+
+To update multiple records in a database:
+•	coll.update_many({"nationality": "american"},
+                    {"$set": {"hair_colour": "maroon"}})
+•	The update_many, will update any record it
+    reads with the nationality of americn.
+
+"""
+
+# Update multiple records:
+coll.update_many({"nationality": "american"}, {"$set": {"hair_colour": "maroon"}})
+
+
+documents = coll.find({"nationality": "american"})
 # This will return a mongoDB object aka a cursor.
 for doc in documents:
     print(doc)
